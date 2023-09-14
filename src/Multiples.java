4324c1b0c32d89b64bef6612e9cfcae20f64fd9b
jinyeong617 <119543270+jinyeong617@users.noreply.github.com>
2023-09-13 21:25:49 -0400

    Task

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,33 +1,15 @@
 public class Multiples {
     public static void main(String[] args) {
 
-        int i = 1;
         int j = 0;
-        while (i < 1000);{
+        for (int i = 1; i < 7; i++) {
 
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-
-            if (divisibleBy3 && divisibleBy5) {
+            if ((i % 3 == 0) || (i % 5 == 0)) {
                 j = j + 1;
-                i = i + 1;
-
-            } else if (divisibleBy3) {
-                j = j + 1;
-                i = i + 1;
-
-            } else if (divisibleBy5) {
-                j = j + 1;
-                i = i + 1;
-
-            } else {
-                i = i + 1;
-
             }
-
-            System.out.println(j);
 
 
         }
+        System.out.println(j);
     }
 }
---
+++ src/Reduce.java
@@ -0,0 +1,20 @@
+public class Reduce {
+    public static void main(String[] args) {
+
+        int i = 100;
+        int j = 0;
+        while (i != 0) {
+
+            if (i % 2 == 0) {
+                i = i / 2;
+            } else {
+                i = i - 1;
+            }
+            j += 1;
+        }
+
+
+        System.out.println(j);
+    }
+}
+
